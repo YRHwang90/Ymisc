9548ffbe7fb5b7a5b324017555fb4bc1f5e911cf
YRHwang90 <70814226+YRHwang90@users.noreply.github.com>
2023-10-06 18:23:57 +0900

    1

--- R/long2wide.R
+++ R/long2wide.R
@@ -1,20 +1,25 @@
 
-wide2long<-function(data,
+long2wide<-function(data,
                     ID="ID",
                     separator="_",
                     hemisphere="prefix",
                     start,
                     end){
 
+  if(hemisphere!="prefix"){
+    stop("Hemisphere indicator should be the prefix.")
+  }else {
 
-  ID<-data[,-c(start:end)]
 
-  reshaped_data <- reshape(results,
-                           idvar = colnames(ID),
-                           timevar = "region",
-                           direction = "wide",
-                           sep=separator)
+    ID<-data[,-c(start:end)]
 
-  data<- reshaped_data[order(reshaped_data[,ID]),]
-return(data)
+    reshaped_data <- reshape(results,
+                             idvar = colnames(ID),
+                             timevar = "region",
+                             direction = "wide",
+                             sep=separator)
+
+    data<- reshaped_data[order(reshaped_data[,ID]),]
+    return(data)
+  }
   }
--- R/wide_to_long.R
+++ R/wide_to_long.R
@@ -6,13 +6,21 @@ wide2long<-function(data,
                     start,
                     end){
 
-  d<-reshape(data=data,
-             direction="long",
-             idvar=ID,
-             varying=start:end,sep=separator,
-             timevar="region")
+  if(hemisphere!="prefix"){
+    stop("Hemisphere indicator should be the prefix.")
+  } else{
+
+    d<-reshape(data=data,
+                    direction="long",
+                    idvar=ID,
+                    varying=start:end,
+                    sep=separator,
+                    timevar="region")
 
   data<-d[order(d[,ID]),]
+
+
+  }
 
   return(data)
 
@@ -24,6 +32,7 @@ results<-wide2long(data=sample_data,
                    ID="ID",
                    separator="_",
                    start=2,
-                   end=15
+                   end=15,
+                   hemisphere="f"
                    )
 
